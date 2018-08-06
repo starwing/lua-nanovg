88fa0892c5e9d8083dfebb695e4ef261c6860f11
ionut stoica <stoica.ionut@gmail.com>
2018-08-06 14:27:59 +0200

    adapt canvas file to use moonglfw

--- canvas.lua
+++ canvas.lua
@@ -1,11 +1,13 @@
-local glfw = require "glfw"
+local glfw = require "moonglfw"
 local nvg = require "nvg"
 local canvas = {}
 
 function canvas.open(w, h, title)
-   local win = assert(glfw.window(w, h, title))
-   win:makecurrent()
+   w = glfw.create_window(w, h, title)
+   glfw.make_context_current(w)
    local ctx = nvg.new "antialias"
+   local win = {}
+   win.handle = w
    win.context = ctx
    win.loop   = canvas.loop
    win.update = canvas.update
@@ -19,35 +21,34 @@ end
 
 function canvas:loop(interval)
    local ctx = self.context
-   interval = interval or (1000.0/60.0)
-
-   local t0 = glfw.time()
+   local t0 = glfw.get_time()
    local prevt = t0
-   self.shouldUpdate = true
-   while not self:shouldclose() do
-      if self:key "esc" then break end
-      local ww, wh = self:size()
-      local pw, _ = self:fbsize()
-      local ratio = pw/ww
-
-      if self.onupdate then
-         local nt = glfw.time()
-         self:onupdate(nt - t0, nt - prevt)
-         prevt = nt
-      end
-
-      if self.ondraw and self.shouldUpdate then
-         ctx:beginFrame(ww, wh, ratio)
-         self:ondraw(ctx)
-         ctx:endFrame()
-         self:swapbuffers()
-         self.shouldUpdate = false
-      end
-
-      glfw.wait(interval)
+   interval = interval or (1000.0/60.0)
+   w = self.handle
+   context = self.context
+   while not glfw.window_should_close(w) do
+     if glfw.get_key(self.handle, "escape") == 'press' then break end
+     ww, wh = glfw.get_window_size(w)
+     local pw, _ = glfw.get_framebuffer_size(w)
+     local ratio = pw/ww
+     -- onupdate hook
+     if self.onupdate then
+        local nt = glfw.get_time()
+        self:onupdate(nt - t0, nt - prevt)
+        prevt = nt
+     end
+     -- ondraw hook
+     if self.ondraw then
+        context:beginFrame(ww, wh, ratio)
+        self.ondraw(context)
+        context:endFrame()
+        glfw.swap_buffers(w)
+        self.shouldUpdate = false
+     end
+     glfw.wait_events_timeout(interval)
+     glfw.poll_events()
    end
    return self
 end
 
 return canvas
-
